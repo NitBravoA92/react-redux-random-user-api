10029879addc36ffec88d9be2f00924498426f37
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-07-21 20:59:10 -0400

    Add css styles and fix linters issues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import UsersList from './components/UsersList';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Welcome to React - Redux
+      Welcome to React - Redux Exercise.
       <UsersList />
     </div>
   );
